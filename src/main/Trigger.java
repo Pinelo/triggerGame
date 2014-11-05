b2e55a88c7f068ef703a76203234b9634d548e65
DELL <DELL@INSPIRON>
2014-11-04 21:18:01 -0600

    Funciones principales del juego

--- src/main/Trigger.java
+++ src/main/Trigger.java
@@ -6,17 +6,125 @@
 
 package main;
 
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+
 /**
  *
  * @author Casa
  */
-public class Trigger {
+public class Trigger extends JFrame implements Runnable, KeyListener
+        , MouseListener {
+    
+    public Trigger(){
+        init();
+        start();
+    }
 
-    /**
-     * @param args the command line arguments
+    public void start() {
+        // Declaras un hilo
+        Thread th = new Thread (this);
+        // Empieza el hilo
+        th.start ();
+    }
+    
+    public void init(){
+        this.setSize(800,600);
+    }
+    
+    @Override
+    public void run () {
+        // se realiza el ciclo del juego en este caso nunca termina
+        while (true) {
+            /* mientras dure el juego, se actualizan posiciones de jugadores
+               se checa si hubo colisiones para desaparecer jugadores o corregir
+               movimientos y se vuelve a pintar todo
+            */ 
+            actualiza();
+            checaColision();
+            repaint();
+            try	{
+                // El thread se duerme.
+                Thread.sleep (20);
+            }
+            catch (InterruptedException iexError)	{
+                System.out.println("Hubo un error en el juego " + 
+                        iexError.toString());
+            }
+	}
+    }
+    
+    public void actualiza(){
+        
+    }
+    
+    public void checaColision(){
+        
+    }
+    
+        /**
+     * update
+     * 
+     * Metodo sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo lo que hace es actualizar el contenedor y 
+     * define cuando usar ahora el paint
+     * @param graGrafico es el <code>objeto grafico</code> usado para dibujar.
+     * 
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public void paint (Graphics graGrafico){
+        // Inicializan el DoubleBuffer
+       
+    }
+    
+    /**
+     * paint
+     * 
+     * Metodo sobrescrito de la clase <code>Applet</code>,
+     * heredado de la clase Container.<P>
+     * En este metodo se dibuja la imagen con la posicion actualizada,
+     * ademas que cuando la imagen es cargada te despliega una advertencia.
+     * @param g es el <code>objeto grafico</code> usado para dibujar.
+     * 
+     */
+    public void paint1(Graphics g) {
+    
+    }
+
+    @Override
+    public void keyTyped(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent ke) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent ke) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
     }
     
 }
