67fe280143126f91ec609998a33197d888ece972
DELL <DELL@INSPIRON>
2014-11-04 21:18:39 -0600

    Clases main y de animación

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Marcel
+ */
+public class Main {
+        public static void main(String[] args) {
+            Trigger jfrProbando = new Trigger();
+            jfrProbando.setVisible(true);
+            jfrProbando.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       
+    }
+}
